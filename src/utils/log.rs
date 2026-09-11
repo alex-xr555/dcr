d6bf6866bf3ca0fd40ab0f2b9caf97697e4b3709
alex-xr555 <alexxar555@gmail.com>
2026-09-11 20:10:17 +0300

    Absolutely all logs have been replaced with the new system on log.

--- src/utils/log.rs
+++ src/utils/log.rs
@@ -15,9 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pub fn warn(_: &str) {}
-pub fn error(_: &str) {}
-
 use anstyle::{AnsiColor, Style};
 use env_logger::*;
 use log::*;
